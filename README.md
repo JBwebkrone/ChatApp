c3238fd4185c7c45c3984f644a88477d3450d28c
officialtech <officialtech@protonmail.com>
2021-09-24 12:53:39 +0530

    Design fix

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Chat
 Django based live chat support application using Websockets and Channels
+
+## To start a Redis server on port 6379, run the following command
+docker run -p 6379:6379 -d redis:5
--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings # for development only
+from django.conf.urls.static import static # for development only
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chat/', include('chatapp.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # for development only
